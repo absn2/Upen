8f944b1f4b5310be3fff4ce8626a5cddadd77333
Antonio Barros da Silva Netto <absn2@cin.ufpe.br>
2020-11-08 18:56:31 -0300

    feat(relatorio_de_pesquisa): Adicionado arquivos da base dos cenarios e mock das dependencias

--- upen-gui/src/app/services/HistoricoService/historico.service.ts
+++ upen-gui/src/app/services/HistoricoService/historico.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { Observable } from 'rxjs';
-import { retry, map } from 'rxjs/operators';
+import { Observable} from 'rxjs';
+import { retry } from 'rxjs/operators';
 
 import { Historico } from '../../../../../common/historico';
 import { Pneu } from '../../../../../common/pneu';
@@ -16,5 +16,27 @@ export class HistoricoService {
     private servURL = 'http://localhost:3000';
 
     constructor(private http: HttpClient) {}
+
+    getHistoricos(): Observable<Historico[]> {
+      return this.http.get<Historico[]>(this.servURL + "/historicos")
+                  .pipe(
+                    retry(2)
+      );
+
+    }
+
+    getVeiculos(): Observable<Veiculo[]> {
+      return this.http.get<Veiculo[]>(this.servURL + "/veiculosMock")
+                .pipe(
+                  retry(2)
+              );
+    }
+
+    getPneus(): Observable<Pneu[]> {
+      return this.http.get<Pneu[]>(this.servURL + "/pneusMock")
+                .pipe(
+                  retry(2)
+              );
+    }
 
 }
